e4fa452a78c16342092cade7c15abfdca20fe63f
been1118 <dsin1118@kakao.com>
2025-04-06 19:36:34 +0900

    refactor: import 정리

--- src/main/java/golf/flogbackend/domain/flightLog/dto/SaveFlightLogStepTwoRequestDto.java
+++ src/main/java/golf/flogbackend/domain/flightLog/dto/SaveFlightLogStepTwoRequestDto.java
@@ -1,23 +1,4 @@
 package golf.flogbackend.domain.flightLog.dto;
 
-import golf.flogbackend.domain.member.dto.LoginRequestDto;
-import golf.flogbackend.domain.member.entity.Member;
-import golf.flogbackend.security.jwt.JwtUtil;
-import golf.flogbackend.security.jwt.TokenDto;
-import golf.flogbackend.security.userDetails.UserDetailsImpl;
-import jakarta.mail.MessagingException;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.security.InvalidParameterException;
-import java.time.Duration;
-import java.util.UUID;
-
 public class SaveFlightLogStepTwoRequestDto {
 }
