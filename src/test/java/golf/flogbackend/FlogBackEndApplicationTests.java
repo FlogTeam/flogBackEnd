4dd6f660ce5e8b922540c8d0e0ae1b7db2f85695
been1118 <dsin1118@kakao.com>
2025-04-06 19:27:14 +0900

    test: FlogBackEndApplicationTests class 생성

---
+++ src/test/java/golf/flogbackend/FlogBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package golf.flogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlogBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
