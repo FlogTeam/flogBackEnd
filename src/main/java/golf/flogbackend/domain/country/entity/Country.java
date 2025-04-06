88f792531437f3f70dd6f5cf74adcb01eba18c4a
been1118 <dsin1118@kakao.com>
2025-04-06 19:25:19 +0900

    feat: Country Entity 작성

---
+++ src/main/java/golf/flogbackend/domain/country/entity/Country.java
@@ -0,0 +1,17 @@
+package golf.flogbackend.domain.country.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Country {
+    @Id
+    private String countryCode;
+    @Column
+    private String countryName;
+    @Column
+    private String countryNameKorean;
+}
