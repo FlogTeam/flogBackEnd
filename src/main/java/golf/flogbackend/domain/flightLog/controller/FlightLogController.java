e3ee217ca3530b7f38971b64827015ae8a8b9760
been1118 <dsin1118@kakao.com>
2025-04-07 11:55:56 +0900

    refactor: api mapping refactoring

--- src/main/java/golf/flogbackend/domain/flightLog/controller/FlightLogController.java
+++ src/main/java/golf/flogbackend/domain/flightLog/controller/FlightLogController.java
@@ -10,14 +10,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class FlightLogController {
     private final FlightLogService flightLogService;
 
-    @PostMapping("/api/flightlog")
+    @PostMapping("/flight-log")
     public ResponseEntity<FlightLogResponseDto.AggregateDto> addFlightLog(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                              @RequestBody SaveFlightLogStepOneRequestDto saveFlightLogStepOneRequestDto) throws ParseException {
         return flightLogService.saveFlightLogStepOne(userDetails.getMember(), saveFlightLogStepOneRequestDto);
